0d5ca0921509cbaec5ce96a61dd649c47f3900fb
Moez <moez_zouari@hotmail.fr>
2021-06-23 16:02:28 +0100

    Using an Action Creator Thunk

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart-slice";
 
 let isInitial = true;
 
@@ -16,61 +16,14 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pendind",
-          title: "Sending ...",
-          message: "Sending cart data",
-        })
-      );
-      const response = await fetch(
-        "https://react-http-1e59e-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed");
-      }
-
-      // if (!response.ok) {
-      //   dispatch(
-      //     uiActions.showNotification({
-      //       status: "error",
-      //       title: "Error !",
-      //       message: "Sendig data failed !",
-      //     })
-      //   );
-      // }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success !",
-          message: "Sent cart data successfully",
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    // En cas de manipulation d'erreur avec throw new error
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error !",
-          message: "Sendig data failed !",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
+
   return (
     <Fragment>
       {notification && (
